b861492249d27973b1c1f6b8c7bd30711d233fb8
alexandercoabad <144318238+alexandercoabad@users.noreply.github.com>
2025-12-13 22:28:18 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,7 +10,8 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    clock = Clock(dut.clk, 39, units="ns")
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -18,8 +19,6 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
@@ -27,22 +26,15 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
 
-    # the first scanline on the first frame will have some uninitialized data
-    # (since necessary regs for rendering are computed during the hsync of the
-    # previous scanline); skip it
-    await ClockCycles(dut.clk, 640+48+16+96)
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
 
-    # hsync and vsync should be de-asserted (high)
-    assert dut.uo_out[3].value == 0
-    assert dut.uo_out[7].value == 1
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 0
 
-    # hsync should go low after the front porch
-    await ClockCycles(dut.clk, 640+16+2)
-    assert dut.uo_out[7].value == 0
-
-    # and high again
-    await ClockCycles(dut.clk, 96)
-    assert dut.uo_out[7].value == 1
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
