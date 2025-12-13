fa8ad548db27d283de9684b40d391b7ee4e9b800
alexandercoabad <144318238+alexandercoabad@users.noreply.github.com>
2025-12-13 22:40:07 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  ChristmasTree_MaligayangPasko user_project (
+  tt_um_ChristmasTree_MaligayangPasko user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
